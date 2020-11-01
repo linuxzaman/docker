43a6aec6ba5ba992ca8559432f6bbcabb81d65b3
linuxzaman <47196580+linuxzaman@users.noreply.github.com>
2020-10-31 23:48:51 -0700

    added sort functions

--- containerIP.sh
+++ containerIP.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker inspect $(docker ps -aq)|grep -E 'Hostname|IPAddress'|grep -Ev 'HostnamePath|SecondaryIPAddresses'|sed -e 's/"//g' -e 's/Hostname://g' -e 's/IPAddress://g' -e 's/[[:space:]]//g' -e 's/,//g'|uniq|awk 'NR%2==0 {print $0"    "p;} NR%2 {p=$0;}'
+docker inspect $(docker ps -aq)|grep -E 'Hostname|IPAddress'|grep -Ev 'HostnamePath|SecondaryIPAddresses'|sed -e 's/"//g' -e 's/Hostname://g' -e 's/IPAddress://g' -e 's/[[:space:]]//g' -e 's/,//g'|uniq|awk 'NR%2==0 {print $0"    "p;} NR%2 {p=$0;}'|sort
